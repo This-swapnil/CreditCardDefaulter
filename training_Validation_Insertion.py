7630f61bc8af14fe62106b780a2c0f76ccf1a984
swapnil sonawane <sswapnil0098@gmail.com>
2022-01-13 07:43:26 +0530

    EDA done

--- training_Validation_Insertion.py
+++ training_Validation_Insertion.py
@@ -1,6 +1,8 @@
+from DataTransform_Training.DataTransformation import dataTransform
 from Training_Raw_data_validation.rawValidation import Raw_Data_validation
 
 
 class train_validation():
     def __init__(self):
         self.raw_data = Raw_Data_validation()
+        self.dataTransform = dataTransform()
